744e0e27b4587c6d82a17dd4a88ae7404c815b5c
Céline <celinesou77@gmail.com>
2024-02-15 12:17:43 +0100

    exo 2 fini

--- exo_2/script.js
+++ exo_2/script.js
@@ -1,3 +1,9 @@
+let numberToGuess;
+let givenNumber;
+let minRange = 0;
+let maxRange = 50;
+let guessCount = 0;
+
 function askGivenNumber(str) {
    return prompt(str);
 }
@@ -13,30 +19,76 @@ function askGuessedNumber(str) {
 
 function didIWin(nPlayer1, nPlayer2) {
    if (nPlayer1 === nPlayer2) {
-      alert("Bravo ! Vous avez deviné le nombre");
+      //alert("Bravo ! Vous avez deviné le nombre");
       return true;
    }
-   if (nPlayer1 > nPlayer2) alert("Plus grand");
-   else if (nPlayer1 < nPlayer2) alert("plus petit");
+   //if (nPlayer1 > nPlayer2) alert("Plus grand");
+   //else if (nPlayer1 < nPlayer2) alert("plus petit");
    return false;
 }
 
-function display() {
-   
+function modifyRange(n) {
+   if (n > minRange && n < numberToGuess) {
+      minRange = n;
+      document.getElementById("min").innerText = `${minRange} < `;
+   }
+   if (n < maxRange && n > numberToGuess) {
+      maxRange = n;
+      document.getElementById("max").innerText = ` < ${maxRange}`;
+   }
+   if (n < minRange || n > maxRange) alert(`Le nombre doit être entre ${minRange} et ${maxRange} !`);
 }
 
-let numberToGuess;
-let givenNumber;
+function displayCount(n) {
+   let displayedCount = document.getElementById("count");
 
-function gameplay() {
-   if (numberToGuess === undefined) numberToGuess = parseInt(askGuessedNumber("Joueur 1: Donnez un nombre entre 0 et 50"))
-   givenNumber = parseInt(askGivenNumber("Joueur 2: Devinez un nombre entre 0 et 50"));
-
-   console.log(numberToGuess, givenNumber);
-
-   if (didIWin(numberToGuess, givenNumber) === false) {
-      gameplay();
+   if (displayedCount) displayedCount.innerText = `Nombre de tentatives : ${n}`;
+   else {
+      let countNode = document.createElement("p")
+      countNode.setAttribute("id", "count");
+      document.body.append(countNode);
+      displayCount(n);
    }
 }
 
-gameplay();
+function displayWin() {
+   document.getElementById("main").setAttribute("hidden", true);
+   let displayedMessage = document.createElement("p");
+   displayedMessage.setAttribute("id", "win");
+   document.body.append(displayedMessage);
+   displayedMessage.innerText = "Bravo ! Vous avez deviné le nombre !";
+
+}
+
+function getNumber() {
+   const res = parseInt(document.getElementById("guess").value);
+   document.getElementById("guess").value = "";
+   return res;
+}
+
+function gameplay() {
+   //if (numberToGuess === undefined) numberToGuess = parseInt(askGuessedNumber("Joueur 1: Donnez un nombre entre 0 et 50"))
+   //givenNumber = parseInt(askGivenNumber("Joueur 2: Devinez un nombre entre 0 et 50"));
+
+   console.log("hello");
+
+   if (numberToGuess === undefined) {
+      numberToGuess = getNumber();
+      return;
+   }
+
+   givenNumber = getNumber();
+
+   console.log(numberToGuess, givenNumber)
+
+   
+   if (didIWin(numberToGuess, givenNumber) === false) {
+      guessCount++;
+      displayCount(guessCount);
+      modifyRange(givenNumber);
+   } else {
+      displayWin();
+   }
+}
+
+//gameplay();
