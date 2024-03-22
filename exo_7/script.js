e6e0bcb371df9cbb43aa7a06872b7f17be0d0f8d
Céline <celinesou77@gmail.com>
2024-03-22 13:49:50 +0100

    exo 7 - étape 6, mise en page du jeu

--- exo_7/script.js
+++ exo_7/script.js
@@ -4,6 +4,15 @@ function removeMatches(n) {
    matches -= n;
 }
 
+function getNumberOfPlayer() {
+   let res = parseInt(prompt("Combien de joueur ?"));
+   while (isNaN(res) || res < 1) {
+      alert("Nombre invalide !");
+      res = parseInt(prompt("Combien de joueur ?"));
+   }
+   return res;
+}
+
 function getMatchesNumber() {
    const matchesSelect = document.getElementById("matches-number");
    const options = matchesSelect.options;
@@ -11,8 +20,11 @@ function getMatchesNumber() {
    const res = options[selectedOptionIndex].value;
 
    if (res < 1 || res > 6 || res > matches) {
-      if (matches === 1) alert("Il reste une allumette !")
-      else alert(`Choisissez un nombre entre 1 et ${matches < 6 ? matches : 6} !`);
+      if (matches === 1) alert("Il reste une allumette !");
+      else
+         alert(
+            `Choisissez un nombre entre 1 et ${matches < 6 ? matches : 6} !`
+         );
 
       return 0;
    }
@@ -24,30 +36,26 @@ function didPlayerWin() {
    return matches === 0;
 }
 
-function gameplay(numberOfPlayer) {
-   let turn = 1;
-
-   displayMatchesCount();
-   displayNumberOfPlayers(numberOfPlayer);
-
-   document.getElementById("remove").addEventListener("click", () => {
-      let removedMatchesNumber = getMatchesNumber();
-      removeMatches(removedMatchesNumber);
-      displayMatchesCount();
-
-      if (didPlayerWin()) {
-         alert(`Le joueur ${turn} a gagné !`);
-      }
-
-      if (removedMatchesNumber != 0) {
-         turn = turn < numberOfPlayer ? turn + 1 : 1;
-         displayPlayerTurn(turn);
-      }
-   });
+function displayMatchesCount() {
+   document.getElementById(
+      "matches-count"
+   ).textContent = `Nombre d'allumettes : ${matches}`;
 }
 
-function displayMatchesCount() {
-   document.getElementById("matches-count").textContent = matches;
+function displayMatchesImage() {
+   const matchesImg = document.getElementById("matches-draw");
+
+   if (matchesImg.childNodes.length === 0) {
+      for (let i = 0; i < matches; i++) {
+         const oneMatch = document.createElement("span");
+         oneMatch.textContent = "‖";
+         matchesImg.append(oneMatch);
+      }
+   }
+
+   while (matchesImg.childNodes.length > matches) {
+      matchesImg.removeChild(matchesImg.lastChild);
+   }
 }
 
 function displayNumberOfPlayers(n) {
@@ -57,10 +65,42 @@ function displayNumberOfPlayers(n) {
 }
 
 function displayPlayerTurn(n) {
-   document.getElementById("player-turn").textContent = `Tour du joueur ${n} :`;
+   document.getElementById("player-turn").textContent = `Tour du joueur ${n}`;
+}
+
+function displayWin(player) {
+   const main = document.querySelector("main");
+   main.replaceChildren();
+
+   let message = document.createElement("p");
+   message.classList.add("win-message")
+   message.textContent = `Bravo ! Le joueur ${player} a gagné!`;
+
+   main.append(message);
+}
+
+function gameplay(numberOfPlayer) {
+   let turn = 1;
+
+   displayMatchesCount();
+   displayMatchesImage();
+   displayNumberOfPlayers(numberOfPlayer);
+
+   document.getElementById("remove").addEventListener("click", () => {
+      let removedMatchesNumber = getMatchesNumber();
+      removeMatches(removedMatchesNumber);
+      displayMatchesCount();
+      displayMatchesImage();
+
+      if (didPlayerWin()) {
+         displayWin(turn);
+      } else if (removedMatchesNumber != 0) {
+         turn = turn < numberOfPlayer ? turn + 1 : 1;
+         displayPlayerTurn(turn);
+      }
+   });
 }
 
 /********************************************************************/
 
-displayMatchesCount();
-gameplay(parseInt(prompt("Combien de joueur ?")));
+gameplay(getNumberOfPlayer());
