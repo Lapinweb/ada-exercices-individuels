29698390ccba944bb40451d26b3f79ff238f0b1e
Céline <celinesou77@gmail.com>
2024-04-03 14:37:12 +0200

    exo 9 étape 2

--- exo_9/script.js
+++ exo_9/script.js
@@ -9,30 +9,43 @@ function addSpaces(numberOfSpaces) {
 function addStars(numberOfStars) {
    let res = "";
    for (let i = 0; i < numberOfStars; i++) {
-       res += "*";
+      res += "*";
    }
    return res;
 }
 
 function displayTree(extraFloors) {
    let treeArray = [];
-   let gap = extraFloors + 1; //spaces on the left of each line, decrement by 1 the lower the line
+   let highestFloorStarNumbers = 5;
 
    //star on top of the tree
-   treeArray[0] = addSpaces(gap) + "+";
-   gap--;
+   treeArray[0] = "+";
 
-   //first floor of the tree
-   treeArray[1] = addSpaces(gap) + "/*\\";
-   gap--;
-
-   //next floors of the tree
-   for (let i = 0, floor = 2; i < extraFloors; i++, floor++, gap--) {
-      treeArray[floor] = addSpaces(gap) + "/*" + addStars((floor-1) * 2) + "\\";
+   //floors of the tree
+   for (
+      let floor = 1, starsNumber = 1;
+      floor <= extraFloors + 1;
+      floor++, starsNumber += 2
+   ) {
+      if ((floor-1) % 3 === 0 && floor != 1) { // if previous floor is mutiple of 3
+         starsNumber = highestFloorStarNumbers;
+         highestFloorStarNumbers += 2;
+      }
+      treeArray[floor] = `/${addStars(starsNumber)}\\`;
    }
 
+   //add gap to the left of the tree
+   let gap = 0;
+   if ((extraFloors + 1) % 3 === 1) gap = 2;
+   if ((extraFloors + 1) % 3 === 2) gap = 1;
+      for (let floor = extraFloors + 1; floor >= 0; floor--, gap++) {
+         treeArray[floor] = addSpaces(gap) + treeArray[floor];
+         if (floor - 1 === 0) continue;
+         else if (floor - 1 === 3) gap -= 1;
+         else if ((floor - 1) % 3 === 0) gap -= 2;
+      }
 
    return treeArray.join("\n");
 }
 
-console.log(displayTree(5));
+console.log(displayTree(9));
