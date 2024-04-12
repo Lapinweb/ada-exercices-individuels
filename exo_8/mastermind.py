30d8e3b4adac9e895d8edc59683cf070aa6201c1
Céline <celinesou77@gmail.com>
2024-04-12 11:55:32 +0200

    exo 8 (mastermind) - début d'une interface : boutons + affichage des couleurs

--- exo_8/mastermind.py
+++ exo_8/mastermind.py
@@ -1,87 +1,68 @@
-import random
+from tkinter import *
+from tkinter import ttk
 
-#Functions for gameplay
-def is_valid_color(color_to_check, available_colors_list):
-   for color in available_colors_list:
-      if color == color_to_check:
-         return True
-   return False
 
-def is_valid_combination(combination):
-   for color in combination:
-      if not is_valid_color(color):
-         return False
-   return True
-
-def get_combination_input(n):
-   full_input = []
-   while len(full_input) < n:
-      new_input = input(f"Choose a color from {pawn_colors} : ")
-      while not is_valid_color(new_input, pawn_colors):
-         new_input = input("Invalid input, retry: ")
-      full_input.append(new_input)
-      print("Selected pawns : ", full_input)
-   return full_input
-
-def check_colors(maker_code, breaker_code):
-   check_result = [0, 0] #result [n of pawns correct color & position, n of pawns right color & wrong position]
-   remaining_maker_pawns = [] #remaining colors without pawns at correct position for maker
-   remaining_breaker_pawns = [] #remaining colors without pawns at correct position for breaker
-   
-   # check pawns correct color & position
-   for i, maker_pawn in enumerate(maker_code):
-      if maker_code[i] == breaker_code[i]:
-         check_result[0] += 1
-      else:
-         remaining_maker_pawns.append(maker_code[i])
-         remaining_breaker_pawns.append(breaker_code[i])
-
-   # check pawns right color & wrong position from remaining pawns
-   for maker_pawn in remaining_maker_pawns:
-      for breaker_pawn in remaining_breaker_pawns:
-         if maker_pawn == breaker_pawn:
-            check_result[1] += 1
-            remaining_breaker_pawns.remove(breaker_pawn)
-            break
-
-   
-   return check_result
-   
-def generate_pawns_suit(n):
-   result = []
-   for i in range(n):
-      random_index = random.randint(0, len(pawn_colors) - 1)
-      result.append(pawn_colors[random_index])
-   return result
-
-def game(number_of_pawns):
-   #Manual input of code to guess
-   # print("Codemaker's turn :")
-   # codemaker_input = get_combination_input(number_of_pawns)
-   
-   #Computer generated input of code to guess
-   codemaker_input = generate_pawns_suit(number_of_pawns)
-    
-   #User input
-   print("Codebreaker's turn")
-   codebreaker_input = get_combination_input(number_of_pawns)
-   
-
-   pawn_check = check_colors(codemaker_input, codebreaker_input)
-   
-   while not pawn_check[0] == number_of_pawns:
-      # print("codemaker : ", codemaker_input)
-      # print("codebreaker : ", codebreaker_input)
-      print(pawn_check)
-      print("\nWrong ! Retry : ")
-      codebreaker_input = get_combination_input(number_of_pawns)
-      pawn_check = check_colors(codemaker_input, codebreaker_input)
+class Mastermind:
+   def __init__(self, root) :
+      root.title("Mastermind")
       
-   print("codemaker : ", codemaker_input)
-   print("codebreaker : ", codebreaker_input)
-   print("Congratulations, you won !")
-
-######################################################################
-pawn_colors = ["red", "blue", "yellow", "green","purple", "orange", "pink", "black"]
-
-game(4)
+      style = ttk.Style(root)
+      
+      self.turn = 0
+      self.colors = ["red", "orange", "yellow", "green", "blue", "indigo", "purple", "pink"]
+      self.pawns = []
+      
+      content = ttk.Frame(root, padding="10")
+      content.grid(column=0, row=0)
+      
+      colors_frame = ttk.LabelFrame(content, text="Choose a color", padding="10")
+      colors_frame.grid(column=0, row=0)
+      
+      column_number = 0
+      row_number = 0
+      for color in self.colors:
+         style.configure("Color_%s.TButton" %color, foreground=color)
+         color_button = ttk.Button(colors_frame, text=color.upper(), width=10, padding="5", style="Color_%s.TButton" %color,
+         command=lambda selected_color = color: self.add_pawn(selected_color))
+         color_button.grid(column=column_number,row=row_number)
+         column_number += 1
+         if column_number > 3:
+            column_number = 0
+            row_number += 1
+      
+      """ color buttons
+      color1 = ttk.Button(colors_frame, text=colors[0]).grid(column=1,row=1)
+      color2 = ttk.Button(colors_frame, text=colors[1]).grid(column=2,row=1)
+      color3 = ttk.Button(colors_frame, text=colors[2]).grid(column=3,row=1)
+      color4 = ttk.Button(colors_frame, text=colors[3]).grid(column=4,row=1)
+      color5 = ttk.Button(colors_frame, text=colors[4]).grid(column=1,row=2)
+      color6 = ttk.Button(colors_frame, text=colors[5]).grid(column=2,row=2)
+      color7 = ttk.Button(colors_frame, text=colors[6]).grid(column=3,row=2)
+      color8 = ttk.Button(colors_frame, text=colors[7]).grid(column=4,row=2) """
+      
+      pawns_frame = ttk.LabelFrame(content, text="Selection", padding="20")
+      pawns_frame.grid(column=0, row=(row_number+1), sticky=(W,E))
+      for index in range(4):
+         pawn_text = index + 1
+         pawn = ttk.Label(pawns_frame, text=pawn_text,anchor="center", width=10, padding="5", borderwidth=2, relief="ridge", style="Pawn_%s.TLabel" %index, background="white")
+         self.pawns.append(pawn)
+         self.pawns[index].grid(column=index,row=3)
+         
+      print(self.pawns)
+      
+      """ pawn labels
+      pawn1 = ttk.Label(pawns_frame, text=1,anchor="center", width=10, padding="5").grid(column=0,row=3)
+      pawn2 = ttk.Label(pawns_frame, text=2,anchor="center", width=10, padding="5").grid(column=1,row=3)
+      pawn3 = ttk.Label(pawns_frame, text=3,anchor="center", width=10, padding="5").grid(column=2,row=3)
+      pawn4 = ttk.Label(pawns_frame, text=4,anchor="center", width=10, padding="5").grid(column=3,row=3) """
+   
+   def add_pawn(self, color):
+      if self.turn < 4:
+         self.pawns[self.turn]["text"] = color.upper()
+         self.pawns[self.turn]["background"] = color
+         self.turn+=1
+   
+      
+root = Tk()
+Mastermind(root)
+root.mainloop()
